871415888979a18df509aa18cda737387ed2f371
Brian Michel <bmoralescutting@gmail.com>
2024-03-18 22:02:57 +0100

    CCM.ConsoleApp

---
+++ CCM.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using CCM.DataAccess.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CCM.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Creando un contexto para interactuar con la Base de datos.
+            ApplicationContext appContext = new ApplicationContext("Data Source=CCMDB.sqlite");
+            // Verificando si la BD no existe
+            if (!appContext.Database.CanConnect())
+            {
+                // Migrando base de datos. Este paso genera la BD con las tablas configuradas en su migración.
+                appContext.Database.Migrate();
+            }
+        }
+    }
+}
