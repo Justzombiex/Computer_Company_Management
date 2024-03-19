1abe9ef75ba02dcfdb3d2c417e6af488dd7a10ec
Betty <betty.ortiz.ramirez0@gmail.com>
2024-03-18 22:33:23 -0400

    Update

--- Computer_Company_Management/Entities/Persons/Worker.cs
+++ Computer_Company_Management/Entities/Persons/Worker.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CCM.Domain.Entities.Common;
+using CCM.Domain.Entities.Types;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,23 @@ using System.Threading.Tasks;
 
 namespace CCM.Domain.Entities.Persons
 {
-    internal class Worker
+    internal class Worker : Person, Entity
     {
+        #region Properties
+        string _WorkerID { get; }
+        JobType Job { get; set; }
+        double Salary { get; set; }
+        #endregion
+
+        #region Constructors
+        protected Worker() { }
+        #endregion
+        public Worker(string cI, string name) : base(cI, name)
+        {
+            _WorkerID = string.Empty;
+            Job = JobType.STORECLERK;
+            Salary = 0;
+
+        }
     }
 }
