6100a254a2c99957e4d200b1db3d941c949e4285
Betty <betty.ortiz.ramirez0@gmail.com>
2024-03-26 10:18:56 -0400

    Pruebas

---
+++ CCM.DataAccess.Tests/CompanyTests.cs
@@ -0,0 +1,85 @@
+﻿using CCM.DataAccess.Abstract.Companies;
+using CCM.DataAccess.Repositories;
+using CCM.DataAccess.Tests.Utilities;
+using CCM.Domain.Entities.Companies;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCM.DataAccess.Tests
+{
+    [TestClass]
+    public class CompanyTests
+    {
+        private ICompaniesRepository _companiesRepository;
+
+        public CompanyTests()
+        {
+            _companiesRepository = new ApplicationRepository(ConnectionStringProvider.GetConnectionString());
+        }
+
+        [DataRow("Microsoft")]
+        [DataRow("Apple")]
+        [TestMethod]
+        public void Can_Create_Company(string name)
+        {
+            _companiesRepository.BeginTransaction();
+
+            Company newCompany = _companiesRepository.Create(name);
+            _companiesRepository.PartialCommit();
+            Company? loadedCompany = _companiesRepository.Get(newCompany.Id);
+            _companiesRepository.CommitTransaction();
+
+            Assert.IsNotNull(loadedCompany);
+            Assert.AreEqual(loadedCompany.CompanyName, name);
+        }
+
+        [DataRow(1)]
+        [DataRow(2)]
+        [TestMethod]
+        public void Can_Get_Company(int id)
+        {
+            _companiesRepository.BeginTransaction();
+
+            var loadedCompany = _companiesRepository.Get(id);
+            _companiesRepository.CommitTransaction();
+
+            Assert.IsNotNull(loadedCompany);
+        }
+        [DataRow(1, "Microsoft")]
+        [DataRow(2, "Apple")]
+        [TestMethod]
+        public void Can_Update_Company(int id, string name)
+        {
+            _companiesRepository.BeginTransaction();
+
+            var loadedCompany = _companiesRepository.Get(id);
+            Assert.IsNotNull(loadedCompany);
+            var newCompany = new Company(name) { Id = loadedCompany.Id };
+            _companiesRepository.Update(newCompany);
+            var modifyedCompany = _companiesRepository.Get(id);
+            _companiesRepository.CommitTransaction();
+
+            Assert.AreEqual(modifyedCompany.CompanyName, name);
+        }
+
+        [DataRow(1)]
+        [TestMethod]
+        public void Can_Delete_Company(int id)
+        {
+            _companiesRepository.BeginTransaction();
+
+            var loadedCompany = _companiesRepository.Get(id);
+            Assert.IsNotNull(loadedCompany);
+            _companiesRepository.Delete(loadedCompany);
+            _companiesRepository.PartialCommit();
+            loadedCompany = _companiesRepository.Get(id);
+            _companiesRepository.CommitTransaction();
+
+            Assert.IsNull(loadedCompany);
+        }
+    }
+}
---
+++ CCM.DataAccess.Tests/ShopTests.cs
@@ -0,0 +1,87 @@
+﻿using CCM.DataAccess.Abstract.Shops;
+using CCM.DataAccess.Concrete;
+using CCM.DataAccess.Repositories;
+using CCM.DataAccess.Tests.Utilities;
+using CCM.Domain.Entities.Shops;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCM.DataAccess.Tests
+{
+    [TestClass]
+    public class ShopTests
+    {
+        private IShopsRepository _shopsRepository;
+
+        public ShopTests()
+        {
+            _shopsRepository = new ApplicationRepository(ConnectionStringProvider.GetConnectionString());
+        }
+
+        [DataRow("TiendaA", "Calle_51")]
+        [DataRow("TiendaB", "Calle_49")]
+        [TestMethod]
+        public void Can_Create_Shop(string name, string address)
+        {
+            Shop newShop = _shopsRepository.Create(name, address);
+            _shopsRepository.PartialCommit();
+            Shop? loadedShop = _shopsRepository.Get(newShop.Id);
+            _shopsRepository.CommitTransaction();
+
+            Assert.IsNotNull(loadedShop);
+            Assert.AreEqual(loadedShop.ShopName, name);
+            Assert.AreEqual(loadedShop.ShopAddress, address);
+        }
+
+        [DataRow(1)]
+        [DataRow(2)]
+        [TestMethod]
+        public void Can_Get_Shop(int id)
+        {
+            _shopsRepository.BeginTransaction();
+
+            var loadedShop = _shopsRepository.Get(id);
+            _shopsRepository.CommitTransaction();
+
+            Assert.IsNotNull(loadedShop);
+        }
+
+        [DataRow(1, "TiendaA", "Calle_51")]
+        [DataRow(2, "TiendaB", "Calle_49")]
+        [TestMethod]
+        public void Can_Update_Shop(int id, string name, string address)
+        {
+            _shopsRepository.BeginTransaction();
+
+            var loadedShop = _shopsRepository.Get(id);
+            Assert.IsNotNull(loadedShop);
+            var newShop = new Shop(name, address) { Id = loadedShop.Id };
+            _shopsRepository.Update(newShop);
+            var modifyedShop = _shopsRepository.Get(id);
+            _shopsRepository.CommitTransaction();
+
+            Assert.AreEqual(modifyedShop.ShopName, name);
+            Assert.AreEqual(modifyedShop.ShopAddress, address);
+        }
+
+        [DataRow(1)]
+        [TestMethod]
+        public void Can_Delete_Shop(int id)
+        {
+            _shopsRepository.BeginTransaction();
+
+            var loadedShop = _shopsRepository.Get(id);
+            Assert.IsNotNull(loadedShop);
+            _shopsRepository.Delete(loadedShop);
+            _shopsRepository.PartialCommit();
+            loadedShop = _shopsRepository.Get(id);
+            _shopsRepository.CommitTransaction();
+
+            Assert.IsNull(loadedShop);
+        }
+    }
+}
