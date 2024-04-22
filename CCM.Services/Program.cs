d405ef20f92602061e922daf935c7594e7ba8697
Brian Michel <bmoralescutting@gmail.com>
2024-04-22 21:15:01 +0200

    Grpc initial commit

---
+++ CCM.Services/Program.cs
@@ -0,0 +1,31 @@
+using CCM.DataAccess.Abstract.Common;
+using CCM.DataAccess.Repositories;
+using CCM.Services.Services;
+
+namespace CCM.Services
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Additional configuration is required to successfully run gRPC on macOS.
+            // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+            // Add services to the container.
+            builder.Services.AddGrpc();
+            builder.Services.AddAutoMapper(typeof(Program).Assembly);
+            builder.Services.AddSingleton("Data Source=Data.sqlite");
+            builder.Services.AddScoped<IPriceRepository, ApplicationRepository>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            app.MapGrpcService<PriceService>();
+            app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+            app.Run();
+        }
+    }
+}
